019628456b3f5a9b46de5728c119008b69f4bca2
Lexfer <lexferramirez@gmail.com>
2025-04-02 11:44:10 -0400

    fix 2

--- src/app/dashboard/solicitud/page.js
+++ src/app/dashboard/solicitud/page.js
@@ -501,7 +501,7 @@ const Page = () => {
     <>
       {!show ? (
         <Container component="main" maxWidth="xl" >
-          <Card elevation={6} sx={{ width: "100%", marginBottom: 3 }}>
+          <Card elevation={6} sx={{ width: "100%", marginBottom: 3,height: '80vh' }}>
             <Box sx={{ p: 3 }}>
               <Typography variant="h6">Gestión de Solicitudes</Typography>
             </Box>
